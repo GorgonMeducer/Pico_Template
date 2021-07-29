e40bb716803e9167211603bde4fa08dcc743aca4
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2021-07-29 17:40:12 +0100

    fixed DebugInSRAM

---
+++ project/mdk/RTE/_AC6-RunInSRAM/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'template' 
+ * Target:  'AC6-RunInSRAM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM0plus_MPU.h"
+
+/* Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+
+
+#endif /* RTE_COMPONENTS_H */
